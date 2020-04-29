55365918a85a753a3222cd5b92b0f6d5b58026d9
dark-orange <42078941+dark-orange@users.noreply.github.com>
2020-04-29 16:58:50 +0300

    Update get_statistics.php

--- get_statistics.php
+++ get_statistics.php
@@ -1,39 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$settings = [
-  'application_key' => '***',
-  'private_key'     => '***',
-  'email'           => '***',
-  'password'        => '***',
-];
-
-$account_id = '***';
-
-
-$client = new Spore('config/route_config.desktop.yaml');
-
-// auth
-$client->enable('AddHeader', [
-  'header_name'  => 'X-Weborama-Account_Id',
-  'header_value' => $account_id
-]);
-$client->enable('Spore_Middleware_Weborama_Authentication', [
-  'application_key' => $settings['application_key'],
-  'private_key'     => $settings['private_key'],
-  'user_email'      => $settings['email']
-]);
-$auth = $client->get_authentication_token([
-  'format'   => 'json',
-  'email'    => $settings['email'],
-  'password' => $settings['password']
-]);
-$client->enable('AddHeader', [
-  'header_name'  => 'X-Weborama-UserAuthToken',
-  'header_value' => $auth->body->token,
-]);
-// end auth
-
+/** auth **/
 
 $res = $client->get_statistics([
   'format'     => 'json',
